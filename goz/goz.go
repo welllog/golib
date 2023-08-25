475791369dc750cbd2d69849d2131044a5e8d93a
welllog <orinfy@foxmail.com>
2023-08-25 10:36:30 +0800

    feat: add goz

---
+++ goz/goz.go
@@ -0,0 +1,107 @@
+package goz
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Limiter struct {
+	c            chan struct{}
+	w            sync.WaitGroup
+	panicHandler func(any)
+}
+
+func NewLimiter(limit int) *Limiter {
+	if limit < 1 {
+		limit = 3
+	}
+	return &Limiter{
+		c: make(chan struct{}, limit),
+	}
+}
+
+func (l *Limiter) SetPanicHandler(fn func(any)) *Limiter {
+	l.panicHandler = fn
+	return l
+}
+
+func (l *Limiter) Go(fn func()) *Limiter {
+	l.add()
+
+	go Recover(fn, l.panicHandler, l.done)
+
+	return l
+}
+
+func (l *Limiter) Wait() {
+	l.w.Wait()
+}
+
+func (l *Limiter) add() {
+	l.c <- struct{}{}
+	l.w.Add(1)
+}
+
+func (l *Limiter) done() {
+	l.w.Done()
+	<-l.c
+}
+
+func Recover(fn func(), panicFn func(any), cleanups ...func()) {
+	defer func() {
+		if p := recover(); p != nil {
+			if panicFn != nil {
+				panicFn(p)
+			} else {
+				var buf strings.Builder
+				buf.Grow(200)
+
+				buf.WriteString(fmt.Sprintf("panic: %v  Traceback:", p))
+				stack(&buf, 4, 6)
+
+				fmt.Println(buf.String())
+			}
+		}
+
+		for _, cleanup := range cleanups {
+			cleanup()
+		}
+	}()
+
+	fn()
+}
+
+func stack(buf *strings.Builder, skip, deep int) {
+	callers := make([]uintptr, deep)
+	n := runtime.Callers(skip, callers)
+	frames := runtime.CallersFrames(callers[:n])
+	for {
+		frame, more := frames.Next()
+		_, _ = buf.WriteString("\n\t")
+		_, _ = buf.WriteString(frame.File)
+		_, _ = buf.WriteString(":")
+		_, _ = buf.WriteString(strconv.Itoa(frame.Line))
+		if !more {
+			break
+		}
+	}
+}
+
+type Logger interface {
+	Error(args ...any)
+}
+
+func LogPanic(l Logger, deep int) func(any) {
+	return func(a any) {
+		var buf strings.Builder
+		buf.Grow(200)
+
+		buf.WriteString(fmt.Sprintf("panic: %v  Traceback:", a))
+		stack(&buf, 5, deep)
+
+		l.Error(buf.String())
+	}
+}
---
+++ goz/goz_test.go
@@ -0,0 +1,107 @@
+package goz
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestRecover(t *testing.T) {
+	var w sync.WaitGroup
+	w.Add(2)
+	go Recover(func() {
+		panic("test1")
+	}, nil, w.Done)
+
+	go Recover(func() {
+		panic("test2")
+	}, func(a any) {
+		t.Log(a)
+	}, w.Done)
+
+	w.Wait()
+}
+
+func TestLimiter_Go(t *testing.T) {
+	var n int32
+
+	limiter := NewLimiter(2).SetPanicHandler(func(a any) {
+		t.Log(a)
+	})
+
+	now := time.Now()
+	limiter.Go(func() {
+		atomic.AddInt32(&n, 1)
+		time.Sleep(time.Second)
+	}).Go(func() {
+		atomic.AddInt32(&n, 1)
+		time.Sleep(time.Second)
+		panic("test panic")
+	}).Go(func() {
+		atomic.AddInt32(&n, 1)
+		if time.Since(now).Milliseconds() < 1000 {
+			t.Fatal("time should more than 1s")
+		}
+		time.Sleep(time.Second)
+	}).Go(func() {
+		atomic.AddInt32(&n, 1)
+		time.Sleep(time.Second)
+	}).Go(func() {
+		atomic.AddInt32(&n, 1)
+		if time.Since(now).Milliseconds() < 2000 {
+			t.Fatal("time should more than 2s")
+		}
+	})
+
+	limiter.Wait()
+	if n != 5 {
+		t.Error("n should be 5")
+	}
+}
+
+func TestLimiter_Go2(t *testing.T) {
+	limiter := NewLimiter(0)
+	limiter.Go(func() {
+		panic(1)
+	}).Go(func() {
+		panic(2)
+	}).Go(func() {
+		panic1(3)
+	}).Wait()
+}
+
+func TestLogPanic(t *testing.T) {
+	f := LogPanic(logger{}, 10)
+
+	var w sync.WaitGroup
+	w.Add(1)
+	go Recover(func() {
+		panic1(1)
+	}, f, w.Done)
+
+	NewLimiter(1).SetPanicHandler(f).Go(func() {
+		panic1(2)
+	}).Wait()
+
+	w.Wait()
+}
+
+type logger struct{}
+
+func (l logger) Error(args ...any) {
+	fmt.Println(args...)
+}
+
+func panic1(n int) {
+	panic2(n)
+}
+
+func panic2(n int) {
+	panic3(n)
+}
+
+func panic3(n int) {
+	panic(n)
+}
