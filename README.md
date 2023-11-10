4ceccfe0825cdf3c48148c69e705bab20075dc20
welllog <orinfy@foxmail.com>
2023-11-10 15:20:13 +0800

    feat: add package dsz

--- README.md
+++ README.md
@@ -6,15 +6,16 @@
 a tool library based on Go standard library, designed for performance, flexibility and simplicity, and enhanced code reuse by introducing generics.
 
 ###
-* ``typez`` constants, variables, type related functions
-* ``slicez`` slice related functions
-* ``strz`` string related functions, able to handle multiple utf8 encoded strings
-* ``hashz`` hash related functions
 * ``cryptz`` encryption and decryption related functions
-* ``randz`` random related functions
-* ``mapz`` map related functions
-* ``testz`` test related functions
-* ``mathz`` math related functions
 * ``ctxz`` ctx related functions
-* ``sortz`` sort related functions
+* ``dsz`` data and structures related types and functions
 * ``goz`` goroutine related functions
+* ``hashz`` hash related functions
+* ``mapz`` map related functions
+* ``mathz`` math related functions
+* ``randz`` random related functions
+* ``slicez`` slice related functions
+* ``sortz`` sort related functions
+* ``strz`` string related functions, able to handle multiple utf8 encoded strings
+* ``testz`` test related functions
+* ``typez`` constants, variables, type related functions
---
+++ dsz/set.go
@@ -0,0 +1,134 @@
+package dsz
+
+// Set[T comparable] is a set of T.
+type Set[T comparable] map[T]struct{}
+
+// Add adds v to s.
+func (s Set[T]) Add(v T) (first bool) {
+	if _, ok := s[v]; ok {
+		return false
+	}
+	s[v] = struct{}{}
+	return true
+}
+
+// MultiAdd adds vs to s.
+func (s Set[T]) MultiAdd(vs ...T) {
+	for _, v := range vs {
+		s[v] = struct{}{}
+	}
+}
+
+// Has reports whether v is in s.
+func (s Set[T]) Has(v T) bool {
+	_, ok := s[v]
+	return ok
+}
+
+// Delete deletes v from s.
+func (s Set[T]) Delete(v T) (exists bool) {
+	if _, ok := s[v]; !ok {
+		return false
+	}
+	delete(s, v)
+	return true
+}
+
+// Len returns the length of s.
+func (s Set[T]) Len() int {
+	return len(s)
+}
+
+// Values returns all values in s.
+func (s Set[T]) Values(dst []T) []T {
+	for k := range s {
+		dst = append(dst, k)
+	}
+	return dst
+}
+
+// Range calls f sequentially for each value present in the set.
+func (s Set[T]) Range(f func(T)) {
+	for k := range s {
+		f(k)
+	}
+}
+
+// Clear clears s.
+func (s Set[T]) Clear() {
+	for k := range s {
+		delete(s, k)
+	}
+}
+
+// Filter deletes all values in s that do not satisfy f.
+func (s Set[T]) Filter(f func(T) bool) {
+	for k := range s {
+		if !f(k) {
+			delete(s, k)
+		}
+	}
+}
+
+// Merge adds all values in other to s.
+func (s Set[T]) Merge(other Set[T]) {
+	for k := range other {
+		s[k] = struct{}{}
+	}
+}
+
+// Diff deletes all values in s that are also in other.
+func (s Set[T]) Diff(other Set[T]) {
+	for k := range other {
+		delete(s, k)
+	}
+}
+
+// Intersect deletes all values in s that are not in other.
+func (s Set[T]) Intersect(other Set[T]) {
+	for k := range s {
+		if _, ok := other[k]; !ok {
+			delete(s, k)
+		}
+	}
+}
+
+// DiffWithSlice deletes all values in s that are also in other.
+func (s Set[T]) DiffWithSlice(other []T) {
+	for _, v := range other {
+		delete(s, v)
+	}
+}
+
+// IntersectWithSlice deletes all values in s that are not in other.
+func (s Set[T]) IntersectWithSlice(other []T) {
+	tmp := make(Set[T], len(other))
+	tmp.MultiAdd(other...)
+
+	for k := range s {
+		if _, ok := tmp[k]; !ok {
+			delete(s, k)
+		}
+	}
+}
+
+// SetFromSlice fills dst with src
+func SetFromSlice[T comparable](dst Set[T], src []T) {
+	for _, v := range src {
+		dst[v] = struct{}{}
+	}
+}
+
+// SetFromMapKeys fills dst with keys from src
+func SetFromMapKeys[T comparable, V any](dst Set[T], src map[T]V) {
+	for k := range src {
+		dst[k] = struct{}{}
+	}
+}
+
+// SetFromMapValues fills dst with values from src
+func SetFromMapValues[T comparable, K comparable](dst Set[T], m map[K]T) {
+	for _, v := range m {
+		dst[v] = struct{}{}
+	}
+}
