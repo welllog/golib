d25cfb5a001ccbb3c7f830ed5ab8b0e6a26bcec6
welllog <orinfy@foxmail.com>
2023-11-25 21:15:41 +0800

    feat: safekv add method

--- mapz/kv.go
+++ mapz/kv.go
@@ -2,15 +2,18 @@ package mapz
 
 type KV[K comparable, V any] map[K]V
 
+// Get returns the value associated with the key and whether the key existed or not.
 func (m KV[K, V]) Get(key K) (V, bool) {
 	value, ok := m[key]
 	return value, ok
 }
 
+// Set sets the value associated with the key.
 func (m KV[K, V]) Set(key K, value V) {
 	m[key] = value
 }
 
+// SetNx sets the value associated with the key if the key does not exist.
 func (m KV[K, V]) SetNx(key K, value V) bool {
 	_, ok := m[key]
 	if !ok {
@@ -19,6 +22,7 @@ func (m KV[K, V]) SetNx(key K, value V) bool {
 	return !ok
 }
 
+// SetX sets the value associated with the key if the key exists.
 func (m KV[K, V]) SetX(key K, value V) bool {
 	_, ok := m[key]
 	if ok {
@@ -27,21 +31,25 @@ func (m KV[K, V]) SetX(key K, value V) bool {
 	return ok
 }
 
+// Delete deletes the value associated with the key.
 func (m KV[K, V]) Delete(keys ...K) {
 	for _, key := range keys {
 		delete(m, key)
 	}
 }
 
+// Has returns whether the key exists.
 func (m KV[K, V]) Has(key K) bool {
 	_, ok := m[key]
 	return ok
 }
 
+// Len returns the number of items.
 func (m KV[K, V]) Len() int {
 	return len(m)
 }
 
+// Keys returns all the keys.
 func (m KV[K, V]) Keys() []K {
 	keys := make([]K, 0, len(m))
 	for k := range m {
@@ -50,6 +58,7 @@ func (m KV[K, V]) Keys() []K {
 	return keys
 }
 
+// Values returns all the values.
 func (m KV[K, V]) Values() []V {
 	values := make([]V, 0, len(m))
 	for _, v := range m {
@@ -58,6 +67,7 @@ func (m KV[K, V]) Values() []V {
 	return values
 }
 
+// Range calls fn sequentially for each key and value present in the map.
 func (m KV[K, V]) Range(fn func(key K, value V) bool) {
 	for k, v := range m {
 		if !fn(k, v) {
--- mapz/safekv.go
+++ mapz/safekv.go
@@ -53,41 +53,6 @@ func (s *SafeKV[K, V]) Set(key K, value V) {
 	s.mu.Unlock()
 }
 
-// Delete deletes the value associated with the key
-func (s *SafeKV[K, V]) Delete(keys ...K) {
-	s.mu.Lock()
-	for _, key := range keys {
-		delete(s.entries, key)
-	}
-	s.mu.Unlock()
-}
-
-// Len returns the length of the map
-func (s *SafeKV[K, V]) Len() int {
-	s.mu.RLock()
-	l := len(s.entries)
-	s.mu.RUnlock()
-	return l
-}
-
-// Range calls fn sequentially for each key and value present in the map.
-func (s *SafeKV[K, V]) Range(fn func(key K, value V) bool) {
-	s.mu.RLock()
-	for k, v := range s.entries {
-		if !fn(k, v) {
-			break
-		}
-	}
-	s.mu.RUnlock()
-}
-
-// Clear clears the map
-func (s *SafeKV[K, V]) Clear() {
-	s.mu.Lock()
-	s.entries = make(map[K]V, len(s.entries))
-	s.mu.Unlock()
-}
-
 // SetNx sets the value associated with the key if the key does not exist
 func (s *SafeKV[K, V]) SetNx(key K, value V) bool {
 	var ok bool
@@ -108,6 +73,71 @@ func (s *SafeKV[K, V]) SetX(key K, value V) bool {
 	}
 	s.mu.Unlock()
 	return ok
+}
+
+// Delete deletes the value associated with the key
+func (s *SafeKV[K, V]) Delete(keys ...K) {
+	s.mu.Lock()
+	for _, key := range keys {
+		delete(s.entries, key)
+	}
+	s.mu.Unlock()
+}
+
+// Has returns whether the key exists
+func (s *SafeKV[K, V]) Has(key K) bool {
+	s.mu.RLock()
+	_, ok := s.entries[key]
+	s.mu.RUnlock()
+	return ok
+}
+
+// Len returns the length of the map
+func (s *SafeKV[K, V]) Len() int {
+	s.mu.RLock()
+	l := len(s.entries)
+	s.mu.RUnlock()
+	return l
+}
+
+// Keys returns all the keys
+func (s *SafeKV[K, V]) Keys() []K {
+	keys := make([]K, 0, len(s.entries))
+	s.mu.RLock()
+	for k := range s.entries {
+		keys = append(keys, k)
+	}
+	s.mu.RUnlock()
+	return keys
+}
+
+// Values returns all the values
+func (s *SafeKV[K, V]) Values() []V {
+	values := make([]V, 0, len(s.entries))
+	s.mu.RLock()
+	for _, v := range s.entries {
+		values = append(values, v)
+	}
+	s.mu.RUnlock()
+	return values
+}
+
+// Range calls fn sequentially for each key and value present in the map.
+func (s *SafeKV[K, V]) Range(fn func(key K, value V) bool) {
+	s.mu.RLock()
+	for k, v := range s.entries {
+		if !fn(k, v) {
+			break
+		}
+	}
+	s.mu.RUnlock()
+}
+
+// Clear clears the map
+func (s *SafeKV[K, V]) Clear() {
+	s.mu.Lock()
+	s.entries = make(map[K]V, len(s.entries))
+	s.mu.Unlock()
 }
 
 // Map calls fn with the map
